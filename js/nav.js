197ef24e109fc818952881833e0ff690e5af07bf
Tanjina Rahman <Tanjinarahman@tanjinasmacbook.fios-router.home>
2020-04-08 16:11:21 -0400

    contact form

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,16 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const lines = document.querySelectorAll('.hamburger div.line');
-const navheight = document.getElementById('nav').offsetHeight;
+
+let navheight = 0;
+const getWidth = () => {
+    const windowWidth = window.innerWidth;
+    if (windowWidth < 768) {
+        navheight = document.getElementById('nav').offsetHeight
+    }
+    return navheight;
+}
+// const navheight = document.getElementById('nav').offsetHeight;
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
@@ -22,5 +31,6 @@ links.forEach(link => {
 
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 600,
+    offset: getWidth,
     updateURL: false
 });
