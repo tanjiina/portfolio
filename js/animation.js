4afc5336a2f7e888c08c7d79c3cf548a35a21e49
Tanjina Rahman <Tanjinarahman@tanjinasmacbook.fios-router.home>
2020-04-14 18:14:21 -0400

    animations

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,7 @@
 gsap.from('.heading', {opacity: 0, duration: 1, y: 50, stagger: .4});
 
-
 gsap.from ('.card', {opacity: 0, duration: 1, y: 50, stagger: .4})
+
+gsap.from ('.sub-section', {opacity: 0, duration: 1, y: 50, stagger: .4})
+
+gsap.from ('.wrapper', {opacity: 0, duration: 1, x: 50, stagger: 2})
